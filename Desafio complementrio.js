4d66a5ffc23afb8b46c3804667386f994216fcd4
Diego da Costa <diegodc169@gmail.com>
2022-07-25 23:16:29 -0300

    subida desafio complementario

---
+++ Desafio complementrio.js
@@ -0,0 +1,5 @@
+let hola = prompt("Ingrese numeros de hola");
+
+for (let i = 0; i < hola; i++) {
+    alert("Hola numero:" + (i + 1));
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+/* let fechaNacimiento = prompt("Ingrese su fecha de nacimiento"); */
+
+let hola = prompt("Ingrese numeros de hola");
+
+for (let i = 0; i < hola; i++) {
+    alert("Hola numero:" + (i + 1));
+}
